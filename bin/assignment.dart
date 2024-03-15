9487b0546845a2ba172e72d8adeccd117621f083
robiul-app <robiulislamweb@gmail.com>
2024-03-15 20:19:02 +0600

    Initial commit

---
+++ bin/assignment.dart
@@ -0,0 +1,28 @@
+class Media {
+  void play() {
+    print("Playing media...");
+  }
+}
+
+class Song extends Media {
+  final String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+void main() {
+
+  Media media = Media();
+
+
+  Song song = Song("The Atif Aslam");
+
+
+  media.play();
+  song.play();
+}
